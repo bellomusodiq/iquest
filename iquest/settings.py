2341eefffbb3976de570816b6e9df1f61b8d2b86
bellomusodiq <bmayowa25@gmail.com>
2020-08-20 13:24:19 +0100

    db settings

--- iquest/settings.py
+++ iquest/settings.py
@@ -82,18 +82,18 @@ WSGI_APPLICATION = 'iquest.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'iquest',
-    #     'USER': 'iquestuser',
-    #     'PASSWORD': 'password',
-    #     'HOST': 'localhost',
-    #     'PORT': '',
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'iquest',
+        'USER': 'iquestuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
 }
 
 # Password validation
