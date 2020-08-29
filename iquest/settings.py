4ba257423a9830c745101af29c86b65e6fb93721
bellomusodiq <bmayowa25@gmail.com>
2020-08-29 21:11:56 +0100

    added s3 cloud storage

--- iquest/settings.py
+++ iquest/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
     'corsheaders',
     'rest_framework',
     'home_template',
@@ -157,4 +158,16 @@ REST_FRAMEWORK = {
     ),
 }
 
-django_heroku.settings(locals())
+
+AWS_ACCESS_KEY_ID = 'AKIAXWCNRVN2I52H5HEO'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'iquest-academy'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'static'
+# STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+DEFAULT_FILE_STORAGE = 'storage_backend.MediaStorage'
